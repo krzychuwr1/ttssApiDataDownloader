aa6577a81ecdf9ce0ef613a5b499c5b4f61d7399
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-11-01 23:48:58 +0100

    Initial commit

---
+++ TramTimes/ApiModel/Model.cs
@@ -0,0 +1,483 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do one of these:
+//
+//    using QuickType;
+//
+//    var routeInfoResponse = RouteInfoResponse.FromJson(jsonString);
+//    var stopPassagesResponse = StopPassagesResponse.FromJson(jsonString);
+//    var stopsResponse = StopsResponse.FromJson(jsonString);
+//    var tripPassagesResponse = TripPassagesResponse.FromJson(jsonString);
+//    var tripInfoResponse = TripInfoResponse.FromJson(jsonString);
+//    var vehicleResponse = VehicleResponse.FromJson(jsonString);
+
+namespace QuickType
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class RouteInfoResponse
+    {
+        [JsonProperty("route")]
+        public Route Route { get; set; }
+
+        [JsonProperty("stops")]
+        public RouteInfoResponseStop[] Stops { get; set; }
+    }
+
+    public partial class StopPassagesResponse
+    {
+        [JsonProperty("actual")]
+        public StopPassagesResponseActual[] Actual { get; set; }
+
+        [JsonProperty("directions")]
+        public object[] Directions { get; set; }
+
+        [JsonProperty("firstPassageTime")]
+        public long FirstPassageTime { get; set; }
+
+        [JsonProperty("generalAlerts")]
+        public object[] GeneralAlerts { get; set; }
+
+        [JsonProperty("lastPassageTime")]
+        public long LastPassageTime { get; set; }
+
+        [JsonProperty("old")]
+        public StopPassagesResponseActual[] Old { get; set; }
+
+        [JsonProperty("routes")]
+        public Route[] Routes { get; set; }
+
+        [JsonProperty("stopName")]
+        public string StopName { get; set; }
+
+        [JsonProperty("stopShortName")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long StopShortName { get; set; }
+    }
+
+    public partial class RouteInfoResponseStop
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("number")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Number { get; set; }
+    }
+
+    public partial class StopPassagesResponseActual
+    {
+        [JsonProperty("actualRelativeTime")]
+        public long ActualRelativeTime { get; set; }
+
+        [JsonProperty("actualTime", NullValueHandling = NullValueHandling.Ignore)]
+        public string ActualTime { get; set; }
+
+        [JsonProperty("direction")]
+        public string Direction { get; set; }
+
+        [JsonProperty("mixedTime")]
+        public string MixedTime { get; set; }
+
+        [JsonProperty("passageid")]
+        public string Passageid { get; set; }
+
+        [JsonProperty("patternText")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long PatternText { get; set; }
+
+        [JsonProperty("plannedTime")]
+        public string PlannedTime { get; set; }
+
+        [JsonProperty("routeId")]
+        public string RouteId { get; set; }
+
+        [JsonProperty("status")]
+        public Status Status { get; set; }
+
+        [JsonProperty("tripId")]
+        public string TripId { get; set; }
+
+        [JsonProperty("vehicleId", NullValueHandling = NullValueHandling.Ignore)]
+        public string VehicleId { get; set; }
+    }
+
+    public partial class Route
+    {
+        [JsonProperty("alerts")]
+        public object[] Alerts { get; set; }
+
+        [JsonProperty("authority")]
+        public string Authority { get; set; }
+
+        [JsonProperty("directions")]
+        public string[] Directions { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Name { get; set; }
+
+        [JsonProperty("routeType")]
+        public RouteType RouteType { get; set; }
+
+        [JsonProperty("shortName")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long ShortName { get; set; }
+    }
+
+    public partial class StopsResponse
+    {
+        [JsonProperty("stops")]
+        public StopElement[] Stops { get; set; }
+    }
+
+    public partial class StopElement
+    {
+        [JsonProperty("category")]
+        public RouteType Category { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("latitude")]
+        public long Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public long Longitude { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("shortName")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long ShortName { get; set; }
+    }
+
+    public partial class TripInfoResponse
+    {
+        [JsonProperty("actual")]
+        public TripPassagesResponseActual[] Actual { get; set; }
+
+        [JsonProperty("directionText")]
+        public string DirectionText { get; set; }
+
+        [JsonProperty("old")]
+        public TripPassagesResponseActual[] Old { get; set; }
+
+        [JsonProperty("routeName")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long RouteName { get; set; }
+    }
+
+    public partial class TripPassagesResponseActual
+    {
+        [JsonProperty("actualTime")]
+        public string ActualTime { get; set; }
+
+        [JsonProperty("status")]
+        public Status Status { get; set; }
+
+        [JsonProperty("stop")]
+        public ActualStop Stop { get; set; }
+
+        [JsonProperty("stop_seq_num")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long StopSeqNum { get; set; }
+    }
+
+    public partial class ActualStop
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("shortName")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long ShortName { get; set; }
+    }
+
+    public partial class VehicleResponse
+    {
+        [JsonProperty("lastUpdate")]
+        public long LastUpdate { get; set; }
+
+        [JsonProperty("vehicles")]
+        public Vehicle[] Vehicles { get; set; }
+    }
+
+    public partial class Vehicle
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("isDeleted", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsDeleted { get; set; }
+
+        [JsonProperty("category", NullValueHandling = NullValueHandling.Ignore)]
+        public RouteType? Category { get; set; }
+
+        [JsonProperty("color", NullValueHandling = NullValueHandling.Ignore)]
+        public Color? Color { get; set; }
+
+        [JsonProperty("tripId", NullValueHandling = NullValueHandling.Ignore)]
+        public string TripId { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("path", NullValueHandling = NullValueHandling.Ignore)]
+        public Path[] Path { get; set; }
+
+        [JsonProperty("longitude", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Longitude { get; set; }
+
+        [JsonProperty("latitude", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Latitude { get; set; }
+
+        [JsonProperty("heading", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Heading { get; set; }
+    }
+
+    public partial class Path
+    {
+        [JsonProperty("length")]
+        public double Length { get; set; }
+
+        [JsonProperty("y1")]
+        public long Y1 { get; set; }
+
+        [JsonProperty("y2")]
+        public long Y2 { get; set; }
+
+        [JsonProperty("x2")]
+        public long X2 { get; set; }
+
+        [JsonProperty("angle")]
+        public long Angle { get; set; }
+
+        [JsonProperty("x1")]
+        public long X1 { get; set; }
+    }
+
+    public enum Status { Departed, Planned, Predicted, Stopping };
+
+    public enum RouteType { Other, Tram };
+
+    public enum Color { The0X000000 };
+
+    public partial class StopPassagesResponse
+    {
+        public static StopPassagesResponse FromJson(string json) => JsonConvert.DeserializeObject<StopPassagesResponse>(json, QuickType.Converter.Settings);
+    }
+
+    public partial class StopsResponse
+    {
+        public static StopsResponse FromJson(string json) => JsonConvert.DeserializeObject<StopsResponse>(json, QuickType.Converter.Settings);
+    }
+
+    public partial class TripPassagesResponse
+    {
+        public static TripInfoResponse FromJson(string json) => JsonConvert.DeserializeObject<TripInfoResponse>(json, QuickType.Converter.Settings);
+    }
+
+    public partial class TripInfoResponse
+    {
+        public static TripInfoResponse FromJson(string json) => JsonConvert.DeserializeObject<TripInfoResponse>(json, QuickType.Converter.Settings);
+    }
+
+    public partial class VehicleResponse
+    {
+        public static VehicleResponse FromJson(string json) => JsonConvert.DeserializeObject<VehicleResponse>(json, QuickType.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this StopPassagesResponse self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+        public static string ToJson(this StopsResponse self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+        public static string ToJson(this TripInfoResponse self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+        public static string ToJson(this VehicleResponse self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                StatusConverter.Singleton,
+                RouteTypeConverter.Singleton,
+                ColorConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+
+    internal class StatusConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Status) || t == typeof(Status?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "DEPARTED":
+                    return Status.Departed;
+                case "PLANNED":
+                    return Status.Planned;
+                case "PREDICTED":
+                    return Status.Predicted;
+                case "STOPPING":
+                    return Status.Stopping;
+            }
+            throw new Exception("Cannot unmarshal type Status");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (Status)untypedValue;
+            switch (value)
+            {
+                case Status.Departed:
+                    serializer.Serialize(writer, "DEPARTED");
+                    return;
+                case Status.Planned:
+                    serializer.Serialize(writer, "PLANNED");
+                    return;
+                case Status.Predicted:
+                    serializer.Serialize(writer, "PREDICTED");
+                    return;
+                case Status.Stopping:
+                    serializer.Serialize(writer, "STOPPING");
+                    return;
+            }
+            throw new Exception("Cannot marshal type Status");
+        }
+
+        public static readonly StatusConverter Singleton = new StatusConverter();
+    }
+
+    internal class RouteTypeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(RouteType) || t == typeof(RouteType?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "other":
+                    return RouteType.Other;
+                case "tram":
+                    return RouteType.Tram;
+            }
+            throw new Exception("Cannot unmarshal type RouteType");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (RouteType)untypedValue;
+            switch (value)
+            {
+                case RouteType.Other:
+                    serializer.Serialize(writer, "other");
+                    return;
+                case RouteType.Tram:
+                    serializer.Serialize(writer, "tram");
+                    return;
+            }
+            throw new Exception("Cannot marshal type RouteType");
+        }
+
+        public static readonly RouteTypeConverter Singleton = new RouteTypeConverter();
+    }
+
+    internal class ColorConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Color) || t == typeof(Color?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            if (value == "0x000000")
+            {
+                return Color.The0X000000;
+            }
+            throw new Exception("Cannot unmarshal type Color");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (Color)untypedValue;
+            if (value == Color.The0X000000)
+            {
+                serializer.Serialize(writer, "0x000000");
+                return;
+            }
+            throw new Exception("Cannot marshal type Color");
+        }
+
+        public static readonly ColorConverter Singleton = new ColorConverter();
+    }
+}
---
+++ TramTimes/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace TramTimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using QuickType;
+
+    class Program
+    {
+        static string MainPath;
+
+        static void Main(string[] args)
+        {
+            MainPath = args[0];
+            Timer_Elapsed(null, null);
+            var timer = new System.Timers.Timer(180000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Start();
+            while (true);
+        }
+
+        private static async void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var ticks = DateTime.Now.Ticks;
+            var directory = System.IO.Directory.CreateDirectory(System.IO.Path.Combine(MainPath, $"time_{ticks}"));
+            var (stops, stopsJson) = await GetStopsAsync();
+            var stopPassages = (await GetStopPassages(stops.Stops)).ToList();
+            var (vehicles, vehiclesJson) = await GetVehicles();
+            var tripPassages = (await GetTripPassages(vehicles.Vehicles.Where(v => !string.IsNullOrWhiteSpace(v.TripId) && !string.IsNullOrWhiteSpace(v.Id)).ToList()));
+            var routeIds = stopPassages.Select(s => s.stops).SelectMany(s => s.Routes).Select(s => s.Id).Distinct();
+            var routes = await GetRouteStops(routeIds);
+
+            var stopPassagesDirectory = System.IO.Directory.CreateDirectory(System.IO.Path.Combine(directory.FullName, "stopPassages"));
+            var tripPassagesDirectory = System.IO.Directory.CreateDirectory(System.IO.Path.Combine(directory.FullName, "tripPassages"));
+            var routeStopsDirectory = System.IO.Directory.CreateDirectory(System.IO.Path.Combine(directory.FullName, "routeStops"));
+
+            await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(directory.FullName, $"stops.json"), stopsJson);
+            await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(directory.FullName, $"vehicles.json"), vehiclesJson);
+
+            foreach(var (stopId, _, json) in stopPassages)
+            {
+                await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(stopPassagesDirectory.FullName, $"stopPassages_{stopId}.json"), json);
+            }
+
+            foreach(var tripPassage in tripPassages)
+            {
+                await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(tripPassagesDirectory.FullName, $"tripPassages_{tripPassage.Key.tripId}_{tripPassage.Key.vehicleId}.json"), tripPassage.Value.json);
+            }
+
+            foreach(var (routeId, _, json) in routes)
+            {
+                await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(routeStopsDirectory.FullName, $"routeStops_{routeId}.json"), json);
+            }
+
+            ////example
+            //var trainStation = stops.Stops.FirstOrDefault(s => s.ShortName == 2690);
+            //var trainStationStopPassages = stopPassages.Select(s => s.stops)
+            //    .Where(s => s.StopShortName == 2690)
+            //    .SelectMany(t => t.Actual)
+            //    .Select(a => (a.TripId, a.VehicleId));
+
+            //var trainStationTripPassages = tripPassages.Where(t => trainStationStopPassages.Contains(t.Key));
+        }
+
+        private static async Task<Dictionary<(string tripId, string vehicleId), (TripInfoResponse deserialized, string json)>> GetTripPassages(List<Vehicle> vehicles)
+        {
+            var tripInfoResponses = await Task.WhenAll(from vehicle in vehicles select GetTripPassages(vehicle.TripId, vehicle.Id));
+
+            return tripInfoResponses.ToDictionary(k => (k.tripId, k.vehicleId), k => (k.tripInfoResponse, k.json));
+        }
+
+        private static async Task<IEnumerable<(string stopId, StopPassagesResponse stops, string json)>> GetStopPassages(IEnumerable<StopElement> stops) 
+            => await Task.WhenAll(from stop in stops select GetStopPassages(stop.ShortName.ToString()));
+
+        static async Task<(VehicleResponse vehicles, string json)> GetVehicles() 
+            => await GetAsync<VehicleResponse>("http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles");
+
+        static async Task<(StopsResponse stops, string json)> GetStopsAsync() 
+            => await GetAsync<StopsResponse>("http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/stopinfo/stops?left=-648000000&bottom=-324000000&right=648000000&top=324000000");
+
+        static async Task<(string stopId, StopPassagesResponse stopPassages, string json)> GetStopPassages(string stopId)
+        {
+            var (deserialized, rawJson) = await GetAsync<StopPassagesResponse>($"http://www.ttss.krakow.pl/internetservice/services/passageInfo/stopPassages/stop?stop={stopId}");
+            return (stopId, deserialized, rawJson);
+        }
+
+        static async Task<(string tripId, string vehicleId, TripInfoResponse tripInfoResponse, string json)> GetTripPassages(string tripId, string vehicleId)
+        {
+            var (deserialized, rawJson) = await GetAsync<TripInfoResponse>($"http://www.ttss.krakow.pl/internetservice/services/tripInfo/tripPassages?tripId={tripId}&vehicleId={vehicleId}");
+            return (tripId, vehicleId, deserialized, rawJson);
+        }
+
+        private static async Task<IEnumerable<(string routeId, RouteInfoResponse routeInfos, string json)>> GetRouteStops(IEnumerable<string> routeIds)
+            => await Task.WhenAll(from routeId in routeIds select GetRouteInfoResponse(routeId));
+
+        static async Task<(string routeId, RouteInfoResponse routeInfoResponse, string json)> GetRouteInfoResponse(string routeId)
+        {
+            var (deserialized, rawJson) = await GetAsync<RouteInfoResponse>($"http://www.ttss.krakow.pl/internetservice/services/routeInfo/routeStops?routeId={routeId}");
+            return (routeId, deserialized, rawJson);
+        }
+        
+        static async Task<(T deserialized, string rawJson)> GetAsync<T>(string path)
+        {
+            int retries = 0;
+            while(true)
+            {
+                try
+                {
+                    HttpClient client = new HttpClient();
+                    HttpResponseMessage response = await client.GetAsync(path);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        return (JsonConvert.DeserializeObject<T>(json, QuickType.Converter.Settings), json);
+                    }
+                    else
+                    {
+                        throw new ApplicationException($"{path} failed, status code: {response.StatusCode}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    retries++;
+                    Debug.WriteLine(e.Message);
+                    if(retries > 10)
+                    {
+                        throw new ApplicationException($"Request failed 10 times: {e.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
